ea9dd603b920f28c9377f948ba47e95ad122dc68
Iskra B <ibatistapoblete@gmail.com>
2019-04-18 23:20:54 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.split(" ")
+  array.each do |name|
+    name[2] = "$" << new_array
+  end
+  new_array
+end
+
+def find_a(array)
+  array.select do |x|
+    x.start_with?("a")
+  end
+end
+
+
+def sum_array(array)
+  array.sum
+end
+
+def add_s(array)
+  new_array = []
+  array.each do |word|
+    word.include?("feet")
+    new_array << "#{word}s"
+  end
+  new_array
+end
